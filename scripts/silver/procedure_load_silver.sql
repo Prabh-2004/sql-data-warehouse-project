31636f600a9e6ee94450816fe72c2eb17dbcdace
Prabh-2004 <prabhjsm@gmail.com>
2026-09-02 21:51:05 +0530

    latest commit

--- scripts/silver/procedure_load_silver.sql
+++ scripts/silver/procedure_load_silver.sql
@@ -1,4 +1,32 @@
--- Finds the Most recent Customers in case of duplicated customers.
+/*
+===============================================================================
+Stored Procedure: Load Silver Layer (Bronze -> Silver)
+===============================================================================
+Script Purpose:
+    This stored procedure performs the ETL (Extract, Transform, Load) process to 
+    populate the 'silver' schema tables from the 'bronze' schema.
+	Actions Performed:
+		- Truncates Silver tables.
+		- Inserts transformed and cleansed data from Bronze into Silver tables.
+		
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+    EXEC Silver.load_silver;
+===============================================================================
+*/
+
+INSERT INTO silver.crm_cust_info (
+	cst_id,
+	cst_key,
+	cst_firstname,
+	cst_lastname,
+	cst_marital_status,
+	cst_gndr,
+	cst_create_date
+)
 SELECT
 cst_id,
 cst_key,
@@ -23,3 +51,58 @@ FROM (
 	WHERE cst_id IS NOT NULL
 ) as t
 WHERE flag_last = 1; -- Select the most recent record per customer
+
+
+INSERT INTO silver.crm_prd_info (
+	prd_id,
+	cat_id,
+	prd_key,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt
+)
+
+SELECT 
+	prd_id,
+	REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') as cat_id, -- Extract Category ID
+	SUBSTRING(prd_key, 7, LEN(prd_key)) as prd_key, -- Extract product key
+	prd_nm,
+	ISNULL(prd_cost, 0) as prd_cost,
+	CASE 
+		WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
+		WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
+		WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
+		WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
+		ELSE 'Unknown'
+	END AS prd_line,  -- Map product line codes to descriptive values.
+	CAST(prd_start_dt AS DATE) AS prd_start_dt,
+	CAST(
+		LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1
+		AS DATE
+	)
+	AS prd_end_dt  -- Calculate end date as one day before the next start date
+FROM bronze.crm_prd_info;
+
+
+SELECT
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+CASE 
+	WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
+	ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+END AS sls_order_dt,
+CASE 
+	WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
+	ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+END AS sls_ship_dt,
+CASE 
+	WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
+	ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+END AS sls_due_dt,
+sls_sales,
+sls_quantity,
+sls_price
+FROM bronze.crm_sales_details
