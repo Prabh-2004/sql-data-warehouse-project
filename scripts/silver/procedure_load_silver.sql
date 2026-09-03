619ff346010f0bdb02d9ebe6bd63f4ec62f0cb68
Prabh-2004 <prabhjsm@gmail.com>
2026-09-03 13:24:16 +0530

    latest commit

--- scripts/silver/procedure_load_silver.sql
+++ scripts/silver/procedure_load_silver.sql
@@ -102,7 +102,15 @@ CASE
 	WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
 	ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
 END AS sls_due_dt,
-sls_sales,
+CASE
+	WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
+		THEN sls_quantity * ABS(sls_price)
+	ELSE sls_sales
+END AS sls_sales,
 sls_quantity,
-sls_price
-FROM bronze.crm_sales_details
+CASE
+	WHEN sls_price IS NULL OR sls_price <= 0
+		THEN sls_sales / NULLIF(sls_quantity, 0)
+	ELSE sls_price
+END AS sls_price
+FROM bronze.crm_sales_details;
